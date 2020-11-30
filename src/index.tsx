0d843c0673e71de2139de1710f65841f2e3af5eb
Deimantas Jakovlevas <hello@zappar.com>
2020-11-30 17:57:54 +0000

    Initial Commit

---
+++ src/index.tsx
@@ -0,0 +1,47 @@
+import { render } from 'react-dom';
+import React, { useRef, useState } from 'react';
+import './style.css';
+import { Canvas, useFrame } from 'react-three-fiber';
+import * as THREE from 'three';
+
+function Box(...props: any[]) { // add your own props type
+  // This reference will give us direct access to the mesh
+  const mesh = useRef<THREE.Mesh>()
+
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => {
+    if (mesh.current) mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+  })
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+
+export default function App() {
+
+    return (
+      <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    );
+}
+
+render(<App />, document.getElementById('root'));
